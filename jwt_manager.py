e450ee0f1fae189567686d7e831cbb9d0710b76c
SantiagoIsmaelFlores-PCPl <santiagoflores@vivaldi.net>
2023-08-03 12:57:37 -0500

    Se implento un codigo mas robusto para la autenticación (comentado)

--- jwt_manager.py
+++ jwt_manager.py
@@ -7,3 +7,28 @@ def create_token(data: dict) -> str:
 def validate_token(token: str) -> dict:
     data: dict = decode(token, key="my_secret_key", algorithm=['HS256'])
     return data
+
+'''
+# Alternativa mas robusta al codigo anterior:
+
+from jwt import encode, decode, InvalidTokenError
+
+def create_token(data: dict) -> str:
+    try:
+        token: str = encode(payLoad=data, key="my_secret_key", algorithm="HS256")
+        return token
+    except Exception as e:
+        print(f"Error creating token: {e}")
+        return ""
+
+def validate_token(token: str) -> dict:
+    try:
+        data: dict = decode(token, key="my_secret_key", algorithm=['HS256'])
+        return data
+    except InvalidTokenError:
+        print("Invalid token")
+        return {}
+    except Exception as e:
+        print(f"Error validating token: {e}")
+        return {}
+'''
